86e78989f180fdfe9814acfbd604fb0b3858fcb9
molzania <molzania_1507@yahoo.co.id>
2022-01-23 12:57:36 +0700

    adding conditional.js

---
+++ control flow/04loop.js
@@ -0,0 +1,26 @@
+/// Soal - 01
+/// Kamu diminta untuk membuat sebuah perulangan menggunakan for loop, untuk mencetak angka prima dari 1 ~ 100
+/// EDIT HERE
+for (a=1; a* a<=100; a++) { 
+    if (a % 1 ===0){
+        
+    }
+}
+
+
+/// Soal - 02
+/// Kamu diminta untuk menemukan bilangan prima ke-50, temukan bilangan tersebut menggunakan while loop 
+let primeCounter = 0;
+let fiftiethPrime;
+
+/// EDIT HERE
+while (....) {}
+
+
+/// Soal - 03
+/// Kamu diminta untuk menemukan bilangan ganjil ke-50, temukan bilangan tersebut menggunakan do while loop
+let oddCounter = 0;
+let fiftiethOdd;
+
+/// EDIT HERE
+do { .... } while (....)
